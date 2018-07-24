daab90f88e8e8b2a8485916d32b18c4bd8103ff0
ingvar <none>
2018-07-24 21:49:44 +0300

    license key fix

--- DSSDK-demo/Main.cs
+++ DSSDK-demo/Main.cs
@@ -9,7 +9,7 @@ namespace DSSDKdemo
         // This is the main entry point of the application.
         static void Main( string[] args )
         {
-            string key = "F1KODdLXTAYpmiZIN+HX3UcxRgF3cy+u6ZNYd6k0KWJlgYKuPhH+3qWs+o14CgrmiB0i8pc1SrvG/UhAAjxahuC8yBz/5uaM0I//jopXv2h3T0k5gOyqmXRTGIBZBIydyrDQwULnp1tNRRwSuy4m7c7BmlAf7aO+jpAqqFvjOD79/7RgeTzAQ9jQrn4Tz33yx6L0WqzZcRxpIVx6ADIJUn5DM33wqHgq/cIqAr+lUUpojUogUTSE1zpjJSrpGzBKxG7BIhLeH3jWpG/u63/zp4RKlSOdJG5wM39wume73GYQ/wG1iGM+DVf0t7av7E+iLNxkQj8D3kjVMNHNumXYHA==";
+            string key = "prpObFZAkWAZNPMGgIA/uMBLJfOj3pAzasxMoglnCmmpCoy5rTE6VqE0Tq6yXy56GafMFiTMeIXtRcgi8MW5Ulmlet889IkpiLRK7rcowpFfJj5yGMnbyI0qXEpIFTqf9X1NwIfNBfpIHrArHCK6HS0P4Jx4w5G8nHvN9sp3yyX9cY67mN9cJKNNN+hPTq+GW9wfn+rd+lLgQUVp5ob5zm/1JHp35TYQq4mq7JpSTbZpS0P7S5YtD61jyXeXpvgik+QIqUUILlPKV2URIuVpm7fH09kanknd33WkDoR0exds04MkyJvzrK7n6+DoQGF1999gA3PzZWtYqkQ00DFM9A==";
 
             PxLicense.InitializeWithKey( key );
 
